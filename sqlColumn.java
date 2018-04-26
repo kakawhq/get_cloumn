a671f76b78c3d9dbcd3943adfaf96205ab9c2022
kaa <kakawhq@gmail.com>
2018-04-26 18:16:55 +0800

    Update sqlColumn.java

--- sqlColumn.java
+++ sqlColumn.java
@@ -1,11 +1,12 @@
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
-//import org.json.JSONObject;
+import java.util.stream.Collectors;
 import py4j.GatewayServer;
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
+
 
 public class sqlColumn {
 
@@ -17,22 +18,21 @@ public class sqlColumn {
         this.dbType = dbType;
     }
 
-    public Map<String, Object> getColumn(String sql) throws Exception {
+    public Map<String, List> getColumn(String sql) throws Exception {
         try {
             List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, dbType);
             SQLStatement stmt = stmtList.get(0);
-
+            
             SchemaStatVisitor statVisitor = SQLUtils.createSchemaStatVisitor(dbType);
             stmt.accept(statVisitor);
-
-            Map<String, Object> dict = new HashMap<String, Object>();
-            //JSONObject dict = new JSONObject();
-            dict.put("tables", statVisitor.getTables().toString());
-            dict.put("columns", statVisitor.getColumns().toString());
-            dict.put("conditions", statVisitor.getConditions().toString());
+            
+            Map<String, List> dict = new HashMap<String, List>();
+            dict.put("tables", statVisitor.getTables().keySet().stream().map(x -> x.toString()).collect(Collectors.toList()));
+            dict.put("columns", statVisitor.getColumns().stream().map(x -> x.toString()).collect(Collectors.toList()));
+            dict.put("conditions", statVisitor.getConditions().stream().map(x -> x.toString()).collect(Collectors.toList()));
             return dict;
-        }
-
+        } 
+        
         catch (Exception e) {
             System.out.println("SOMETHING WRONG");
             throw e;
@@ -40,12 +40,23 @@ public class sqlColumn {
     }
 
     public static void main(String[] args) throws Exception {
-        sqlColumn c1 = new sqlColumn("MYSQL");
-        //Map r1 = c1.getColumn("UPDATE dwd_info_invest2 c,(SELECT ( @i := CASE WHEN @pre_part_no = key_account THEN @i + 1 ELSE 1 END ) row_number, dwd_info_invest.key_primary, ( @pre_part_no := key_account ) FROM dwd_info_invest, ( SELECT @i := 0, @pre_part_no := '' ) AS a WHERE status_invest <> 3 AND type_sourcetable IN ( 1, 2, 4 ) GROUP BY key_account,dt_invest,key_primary ORDER BY key_account ) b SET c.rank_partinvest = b.row_number WHERE c.key_primary = b.key_primary;");
-        //JSONObject res_json = new JSONObject(r1);
-        //System.out.println(r1);
-        GatewayServer server = new GatewayServer(c1, 25334);
+        sqlColumn obj = new sqlColumn("MYSQL");
+        GatewayServer server = new GatewayServer(obj, 25335);
         server.start();
+        /*String testsql = "update dwd_info_invest2 c,\n" +
+                "\t\t(select \n" +
+                "        (@i := case when @pre_part_no=key_account then @i + 1 else 1 end) row_number,     \n" +
+                "\t\t\t\tdwd_info_invest.key_primary,\n" +
+                "        (@pre_part_no:=key_account)        \n" +
+                "    from dwd_info_invest,\n" +
+                "    (SELECT @i := 0, @pre_part_no:='') AS a\n" +
+                "\t  where status_invest <> 3\n" +
+                "    group by key_account, dt_invest, key_primary  \n" +
+                "    order by key_account) b\n" +
+                "set c.rank_allinvest = b.row_number\n" +
+                "where c.key_primary = b.key_primary;";
+        System.out.println(obj.getColumn(testsql));
+        */
     }
 
 }
